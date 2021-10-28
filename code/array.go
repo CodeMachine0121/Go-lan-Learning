f21cf67bed063efde5f8a157bde1c2b4e3feaa0f
薛 華慶 <m10917055@gemail.yuntech.edu.tw>
2021-10-28 10:02:53 +0800

    陣列實作

--- code/array.go
+++ code/array.go
@@ -4,11 +4,13 @@ import "fmt"
 func main(){
 	var ary1 [10]int // 長度可有可無
 	var i int
+	fmt.Print("Array: [")
 	for i = 0; i <10; i++{
 		ary1[i]=i*10
+		fmt.Print(ary1[i]," ")
 	}
 
-	fmt.Println(len(ary1))
+	fmt.Println("]\nlength of array: ",len(ary1))
 	
 	
 }
